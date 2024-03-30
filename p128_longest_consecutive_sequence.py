61e959c0f11880d9ff7d8da56c60a3bc03553bad
notbeckhamster <notbeckhamster@gmail.com>
2024-03-30 21:31:28 +1300

    p128

---
+++ p128_longest_consecutive_sequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+        largestSequence = 0 
+        
+        for num in nums:
+            #check num is start 
+            if num-1 not in nums_set:
+                seqSize = 0
+                curr_num = num
+                while curr_num in nums_set:
+                    seqSize+=1
+                    curr_num+=1
+                if seqSize > largestSequence:
+                    largestSequence = seqSize
+        return largestSequence
+
