2b123234ffa2917a4f406804c5885caf31531597
notbeckhamster <notbeckhamster@gmail.com>
2024-04-05 21:15:57 +1300

    p704

---
+++ p704_binary_serach.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+        while l<=r:
+            m = int(l + (r-l)/2)
+            print(m)
+            m_val = nums[m]
+            if m_val == target:
+                return m
+            elif m_val > target:
+                r = m-1
+            else:
+                l=m+1
+        return -1
