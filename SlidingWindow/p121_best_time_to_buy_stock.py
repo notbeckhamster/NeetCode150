4bf03486784906c3f70deb90f8d28090ab220127
notbeckhamster <notbeckhamster@gmail.com>
2024-04-18 18:02:05 +1200

    p121

---
+++ SlidingWindow/p121_best_time_to_buy_stock.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l,r, max_profit = 0,0,0
+        while r<len(prices):
+            profit = prices[r] - prices[l]
+            max_profit = max(max_profit, profit)
+            if profit < 0:
+                l+=1
+            else:
+                r+=1
+        return max_profit
