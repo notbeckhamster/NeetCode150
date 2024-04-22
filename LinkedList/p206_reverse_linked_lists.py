0d55e26b9e1ac46f792e7de2178019c082376595
notbeckhamster <notbeckhamster@gmail.com>
2024-04-22 20:32:46 +1200

    p206 reverse linkedlist

---
+++ LinkedList/p206_reverse_linked_lists.py
@@ -0,0 +1,16 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr != None:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
